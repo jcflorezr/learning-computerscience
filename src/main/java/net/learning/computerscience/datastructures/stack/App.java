352783310cbd8b5caf77ae9887ceed17b58c9f33
Juan Florez <jcflorezr@gmail.com>
2017-08-08 21:49:18 -0500

    [data-structures][stack] own implementation of Stack class

---
+++ src/main/java/net/learning/computerscience/datastructures/stack/App.java
@@ -0,0 +1,18 @@
+package net.learning.computerscience.datastructures.stack;
+
+public class App {
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(3);
+        stack.push(20);
+        stack.push(40);
+        stack.push(60);
+        stack.push(80);
+
+        while (!stack.isEmpty()) {
+            long value = stack.pop();
+            System.out.println(value);
+        }
+    }
+
+}
