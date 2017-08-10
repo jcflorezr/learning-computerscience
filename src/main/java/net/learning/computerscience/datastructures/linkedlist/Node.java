d5a480c588501f9d53ce00ff69cb237843a403d8
Juan Florez <jcflorezr@gmail.com>
2017-08-09 22:10:32 -0500

    [data-structures][linkedlist] creating a linked list and a singly linked list

---
+++ src/main/java/net/learning/computerscience/datastructures/linkedlist/Node.java
@@ -0,0 +1,8 @@
+package net.learning.computerscience.datastructures.linkedlist;
+
+public class Node {
+
+    int data;
+    Node next = null;
+
+}
---
+++ src/main/java/net/learning/computerscience/datastructures/linkedlist/singlylinkedlist/Node.java
@@ -0,0 +1,12 @@
+package net.learning.computerscience.datastructures.linkedlist.singlylinkedlist;
+
+public class Node {
+
+    public int data;
+    public Node next;
+
+    public void displayNode() {
+        System.out.println("{ " + data + " }");
+    }
+
+}
