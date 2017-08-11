ced65d781306a6f346be7a5e0f68d85e3af90b2a
Juan Florez <jcflorezr@gmail.com>
2017-08-11 12:36:29 -0500

    [data-structures][linked list] implementing doubly linked lists

---
+++ src/main/java/net/learning/computerscience/datastructures/linkedlist/doubly/Node.java
@@ -0,0 +1,13 @@
+package net.learning.computerscience.datastructures.linkedlist.doubly;
+
+public class Node {
+
+    public int data;
+    public Node next;
+    public Node previous;
+
+    public void displayNode() {
+        System.out.println("{ " + data + " }");
+    }
+
+}
