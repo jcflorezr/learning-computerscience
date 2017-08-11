ed97f91d973bcbe83d808ea28b1c21fe620b58a5
Juan Florez <jcflorezr@gmail.com>
2017-08-10 21:19:00 -0500

    [data-structures][linkedlist] implementing circular linked list

---
+++ src/main/java/net/learning/computerscience/datastructures/linkedlist/circularlinkedlist/Node.java
@@ -0,0 +1,12 @@
+package net.learning.computerscience.datastructures.linkedlist.circularlinkedlist;
+
+public class Node {
+
+    int data;
+    Node next = null;
+
+    public void displayNode() {
+        System.out.println("{ " + data + " }");
+    }
+
+}
