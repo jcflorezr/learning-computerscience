349f8cd272cadf6a70d74f54c986621d88ee5713
Juan Florez <jcflorezr@gmail.com>
2017-08-14 10:23:09 -0500

    [data-structures][trees] implementing the Red-Black tree

---
+++ src/main/java/net/learning/computerscience/datastructures/redblacktree/NodeColor.java
@@ -0,0 +1,7 @@
+package net.learning.computerscience.datastructures.redblacktree;
+
+public enum NodeColor {
+
+    RED, BLACK
+
+}
---
+++ src/main/java/net/learning/computerscience/datastructures/redblacktree/Tree.java
@@ -0,0 +1,9 @@
+package net.learning.computerscience.datastructures.redblacktree;
+
+public interface Tree<T> {
+
+    void traverse();
+
+    void insert(T data);
+
+}
