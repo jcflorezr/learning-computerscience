50eaa0775ed7ad5feed99dc84701224bfa36cb8b
Juan Florez <jcflorezr@gmail.com>
2017-08-15 10:17:12 -0500

    [data-structures][hash-tables] implementing hash tables with chaining and linear probing

---
+++ src/main/java/net/learning/computerscience/datastructures/hashtables/linearprobing/App.java
@@ -0,0 +1,21 @@
+package net.learning.computerscience.datastructures.hashtables.linearprobing;
+
+public class App {
+
+    public static void main(String[] args) {
+        HashTable hashTable = new HashTable();
+
+        hashTable.put(1, 10);
+        System.out.println();
+
+        hashTable.put(2, 100);
+        System.out.println();
+
+        hashTable.put(3, 1000);
+        System.out.println();
+
+
+        System.out.println(hashTable.get(2));
+    }
+
+}
---
+++ src/main/java/net/learning/computerscience/datastructures/hashtables/linearprobing/Constants.java
@@ -0,0 +1,7 @@
+package net.learning.computerscience.datastructures.hashtables.linearprobing;
+
+public class Constants {
+
+    public static final int TABLE_SIZE = 10;
+
+}
