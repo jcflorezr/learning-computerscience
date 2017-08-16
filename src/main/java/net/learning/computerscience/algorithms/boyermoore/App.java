19825e45fd85bf1d7793f89e93681f4fa63b75d7
Juan Florez <jcflorezr@gmail.com>
2017-08-15 19:37:56 -0500

    [algorithms][substring-search] implementing Boyer Moore algorithm

--- src/main/java/net/learning/computerscience/algorithms/boyermoore/App.java
+++ src/main/java/net/learning/computerscience/algorithms/boyermoore/App.java
@@ -1,4 +1,14 @@
 package net.learning.computerscience.algorithms.boyermoore;
 
 public class App {
+
+    public static void main(String[] args) {
+        String text = "My name is Juan !!!";
+        String pattern = "M";
+
+        BoyerMoore boyerMoore = new BoyerMoore(text, pattern);
+        boyerMoore.precomputeTable();
+        System.out.println(boyerMoore.search());
+    }
+
 }
--- src/main/java/net/learning/computerscience/algorithms/boyermoore/BoyerMoore.java
+++ src/main/java/net/learning/computerscience/algorithms/boyermoore/BoyerMoore.java
@@ -1,4 +1,50 @@
 package net.learning.computerscience.algorithms.boyermoore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BoyerMoore {
+
+    private String text;
+    private String pattern;
+    private Map<Character, Integer> badMatchTable;
+
+    public BoyerMoore(String text, String pattern) {
+        this.text = text;
+        this.pattern = pattern;
+        this.badMatchTable = new HashMap<>();
+    }
+
+    public void precomputeTable() {
+        int lengthOfPattern = pattern.length();
+
+        for (int i = 0; i < lengthOfPattern; i++) {
+            char actualCharacter = pattern.charAt(i);
+            int maxShift = Math.max(1, lengthOfPattern - i - 1);
+            badMatchTable.put(actualCharacter, maxShift);
+        }
+    }
+
+    public int search() {
+        int lengthOfText = this.text.length();
+        int lengthOfPattern = this.pattern.length(); // O(1)
+        int numOfSkips;
+
+        for (int i = 0; i <= lengthOfText - lengthOfPattern; i = i + numOfSkips) {
+            numOfSkips = 0;
+            for (int j = lengthOfPattern - 1; j >= 0; j--) {
+                if (pattern.charAt(j) != text.charAt(i + j)) {
+                    if (badMatchTable.get(pattern.charAt(j)) != null) {
+                        numOfSkips = badMatchTable.get(pattern.charAt(j));
+                        break;
+                    } else {
+                        numOfSkips = lengthOfPattern;
+                        break;
+                    }
+                }
+            }
+            if (numOfSkips == 0) return i;
+        }
+        return lengthOfText;
+    }
 }
