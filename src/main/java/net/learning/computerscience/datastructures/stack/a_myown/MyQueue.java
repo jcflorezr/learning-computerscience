608416f58cfa0a9df098f40875107340f6a688cf
Juan Florez <jcflorezr@gmail.com>
2017-08-22 21:58:46 -0500

    [data-structures][stack] implementing my own stack

---
+++ src/main/java/net/learning/computerscience/datastructures/stack/a_myown/MyQueue.java
@@ -0,0 +1,89 @@
+package net.learning.computerscience.datastructures.stack.a_myown;
+
+public class MyQueue<T> {
+
+    private Node<T> root;
+    private int size;
+
+    public MyQueue() {
+        root = null;
+    }
+
+    public void push(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (root == null) {
+            root = newNode;
+        } else {
+            newNode.setNext(root);
+            root = newNode;
+        }
+        size++;
+    }
+
+    public T pop() {
+        Node<T> node = null;
+        if (root != null) {
+            node = root;
+            root = node.getNext();
+            node.setNext(null);
+            size--;
+        }
+        return node != null ? node.getData() : null;
+    }
+
+    public T peek() {
+        Node<T> node = null;
+        if (root != null) {
+            node = root;
+        }
+        return (node != null) ? node.getData() : null;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        Node<T> head = root;
+        String result = "[";
+        while (head != null) {
+            result += head + " -> ";
+            head = head.next;
+        }
+        result += head + "]";
+        return result;
+    }
+
+    private class Node <T> {
+
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return data.toString();
+        }
+    }
+
+}
