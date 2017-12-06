cf5a7bc1f8cd2586af69151702eb40f7e6d84b75
Juan Florez <jcflorezr@gmail.com>
2017-12-06 13:45:07 -0500

    [hacker-rank][data-structures][trees] exercise 4

--- src/main/java/net/learning/computerscience/hackerrank/datastructures/trees/Node.java
+++ src/main/java/net/learning/computerscience/hackerrank/datastructures/trees/Node.java
@@ -4,4 +4,29 @@ public class Node {
     public int data;
     public Node left;
     public Node right;
+
+    public Node() {
+    }
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public Node insert(Node root, int data) {
+        if(root == null){
+            return new Node(data);
+        }
+        else {
+            Node cur;
+            if(data <= root.data){
+                cur = insert(root.left, data);
+                root.left = cur;
+            }
+            else{
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
 }
