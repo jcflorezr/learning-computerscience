2b379eaa75ef9be6aaa7b836bf3bfd9ecc7ae36d
Juan Florez <jcflorezr@gmail.com>
2017-12-12 22:44:45 -0500

    [geeks-for-geeks][data-structures][arrays][summary] exercise 13

---
+++ src/main/java/net/learning/computerscience/datastructures/arrays/geeks_for_geeks/summary/p13_array_rotation/FantasticSolution.java
@@ -0,0 +1,24 @@
+package net.learning.computerscience.datastructures.arrays.geeks_for_geeks.summary.p13_array_rotation;
+
+import java.util.Arrays;
+
+/**
+ * http://www.geeksforgeeks.org/array-rotation/
+ */
+public class FantasticSolution {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println(Arrays.toString(leftRotateArray(array, 2)));
+
+    }
+
+    static int[] leftRotateArray(int[] array, int numRotations) {
+        int n = array.length;
+        for(int i = 0; i < n; i++) {
+            array[(i + n - numRotations) % n] = array[i];
+        }
+        return array;
+    }
+
+}
