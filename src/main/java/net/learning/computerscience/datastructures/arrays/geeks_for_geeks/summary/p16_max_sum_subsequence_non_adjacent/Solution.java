2c4b2b02f463197fd652917023b0a1ab9a6dadb6
Juan Florez <jcflorezr@gmail.com>
2017-12-17 11:43:11 -0500

    [geeks-for-geeks][data-structures][arrays][summary] exercise 16

---
+++ src/main/java/net/learning/computerscience/datastructures/arrays/geeks_for_geeks/summary/p16_max_sum_subsequence_non_adjacent/Solution.java
@@ -0,0 +1,24 @@
+package net.learning.computerscience.datastructures.arrays.geeks_for_geeks.summary.p16_max_sum_subsequence_non_adjacent;
+
+/**
+ * http://www.geeksforgeeks.org/maximum-sum-such-that-no-two-elements-are-adjacent/
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 5, 10, 100, 10, 5};
+        System.out.println(findSumMaxSubsequence(arr, 0, 0, 0));
+        arr = new int[]{4, 1, 1, 4, 2, 1};
+        System.out.println(findSumMaxSubsequence(arr, 0, 0, 0));
+        arr = new int[]{1, 2, 3};
+        System.out.println(findSumMaxSubsequence(arr, 0, 0, 0));
+        arr = new int[]{1, 20, 3};
+        System.out.println(findSumMaxSubsequence(arr, 0, 0, 0));
+    }
+
+    private static int findSumMaxSubsequence(int[] arr, int i, int inclusive, int exclusive) {
+        if (i == arr.length) return inclusive;
+        return findSumMaxSubsequence(arr, i + 1, Math.max(exclusive + arr[i], inclusive), inclusive);
+    }
+
+}
