d2b5e92f76ba36eec548de2b1bbec5ae0af9d497
Juan Florez <jcflorezr@gmail.com>
2017-12-17 23:59:55 -0500

    [geeks-for-geeks][data-structures][matrices][summary] exercise 1

---
+++ src/main/java/net/learning/computerscience/datastructures/topic2_matrices/geeks_for_geeks/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <a href="http://www.geeksforgeeks.org/matrix/">
+ *      http://www.geeksforgeeks.org/matrix/
+ * </a>
+ */
+
+package net.learning.computerscience.datastructures.topic2_matrices.geeks_for_geeks;
