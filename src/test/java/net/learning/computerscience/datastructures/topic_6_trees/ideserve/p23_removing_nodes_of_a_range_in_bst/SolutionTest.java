0713c16011ec525a09d357a3050eff6c2cb27946
Juan Flórez <jcflorezr@gmail.com>
2018-05-26 13:08:11 -0500

    [i-deserve][data-structures][trees] preparing excercise 23

--- src/test/java/net/learning/computerscience/datastructures/topic_6_trees/ideserve/p23_removing_nodes_of_a_range_in_bst/SolutionTest.java
+++ src/test/java/net/learning/computerscience/datastructures/topic_6_trees/ideserve/p23_removing_nodes_of_a_range_in_bst/SolutionTest.java
@@ -9,22 +9,29 @@ public class SolutionTest {
     @Test
     public void removeNodesOfASpecificRange() {
 
-        TreeNode treeNode = newBST(new int[]{8, 5, 11, 2, 7, 9, 12, 6, 10, 13}, 0);
+        TreeNode treeNode = newBST(new int[]{8, 5, 11, 2, 7, 9, 12, 6, 10, 13});
         System.out.println();
 
     }
 
-    private TreeNode newBST(int[] elements, int index) {
-        if (index >= elements.length) return null;
-        TreeNode newNode = new TreeNode(elements[index]);
-        TreeNode newNode1 = newBST(elements, index + 1);
-        TreeNode newNode2 = newBST(elements, index + 2);
-        newNode.left = (newNode1 == null ? Integer.MIN_VALUE : newNode1.data) >
-                       (newNode2 == null ? Integer.MIN_VALUE : newNode2.data)
-                    ? newNode2 : newNode1;
-        newNode.right = (newNode1 == null ? Integer.MIN_VALUE : newNode1.data) <
-                    (newNode2 == null ? Integer.MIN_VALUE : newNode2.data)
-                    ? newNode2 : newNode1;
-        return newNode;
+    private TreeNode newBST(int[] elements) {
+        TreeNode root = new TreeNode(elements[0]);
+        for (int i = 1; i < elements.length; i++) {
+            insertNode(elements[i], root);
+        }
+        return root;
+    }
+
+    private void insertNode(int element, TreeNode node) {
+        if (node == null) node = new TreeNode(element);
+        else {
+            if (element <= node.data) {
+                if (node.left == null) node.left = new TreeNode(element);
+                else insertNode(element, node.left);
+            } else {
+                if (node.right == null) node.right = new TreeNode(element);
+                else insertNode(element, node.right);
+            }
+        }
     }
 }
