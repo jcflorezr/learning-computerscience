470229d9e312ba51ac45b876b493846a586b3b50
Juan Flórez <jcflorezr@gmail.com>
2018-06-10 23:23:30 -0500

    [i-deserve][data-structures][trees] exercise 49 - max element from each subarray of size k

---
+++ src/main/java/net/learning/computerscience/datastructures/topic_6_trees/ideserve/p49_max_element_for_each_subarray_of_size_k/Solution.java
@@ -0,0 +1,124 @@
+package net.learning.computerscience.datastructures.topic_6_trees.ideserve.p49_max_element_for_each_subarray_of_size_k;
+
+import java.util.ArrayList;
+
+/**
+ * https://www.ideserve.co.in/learn/maximum-element-from-each-subarray-of-size-k-set-1
+ */
+public class Solution {
+
+
+    // WARNING: AVL BALANCING NOT CORRECTLY IMPLEMENTED
+
+
+    public static ArrayList<Integer> getMaxElements(int[] inputArray, int k) {
+        ArrayList<Integer> elements = new ArrayList<>();
+        TreeNode node;
+        for (int i = 0; i < inputArray.length - (k - 1); i++) {
+            node = insertNodes(getSubArray(inputArray, i, i + k));
+            elements.add(getMaxElement(node));
+        }
+        return elements;
+    }
+
+    private static TreeNode insertNodes(int[] subArray) {
+        TreeNode newTree = null;
+        for (int element : subArray) {
+            newTree = insertNode(newTree, element);
+        }
+        return balanceIfNeeded(newTree);
+    }
+
+    private static TreeNode getPredecessor(TreeNode node) {
+        return node.right == null ? node : getPredecessor(node.right);
+    }
+
+    private static TreeNode insertNode(TreeNode node, int element) {
+        if (node == null) return new TreeNode(element);
+        if (element <= node.data) node.left = insertNode(node.left, element);
+        else if (element > node.data) node.right = insertNode(node.right, element);
+        return node;
+    }
+
+    private static TreeNode balanceIfNeeded(TreeNode current) {
+        if (current == null) return null;
+        current.left = balanceIfNeeded(current.left);
+        current.right = balanceIfNeeded(current.right);
+        int leftHeight = getHeight(current.left);
+        int rightHeight = getHeight(current.right);
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            // left
+            if (leftHeight > rightHeight) {
+                // left
+                if (current.left.left != null) {
+                    TreeNode temp = current.left;
+                    temp.right = current;
+                    current.left = null;
+                    return temp;
+                // right
+                } else {
+                    TreeNode temp = current.left;
+                    TreeNode temp2 = temp.right;
+                    temp2.right = current;
+                    temp2.left = temp;
+                    temp.right = null;
+                    return temp2;
+                }
+            // right
+            } else {
+                // right
+                if (current.right.right != null) {
+                    TreeNode temp = current.right;
+                    temp.left = current;
+                    current.right = null;
+                    return temp;
+                // left
+                } else {
+                    TreeNode temp = current.right;
+                    TreeNode temp2 = temp.left;
+                    temp2.left = current;
+                    temp2.right = temp;
+                    temp.left = null;
+                    return temp2;
+                }
+            }
+        }
+        return current;
+    }
+
+    private static int getHeight(TreeNode node) {
+        if (node == null) return 0;
+        return Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+    }
+
+    private static int getMaxElement(TreeNode node) {
+        return node.right == null ? node.data : getMaxElement(node.right);
+    }
+
+    private static int[] getSubArray(int[] inputArray, int from, int to) {
+        int subArrayLength = to - from;
+        int[] subArray = new int[subArrayLength];
+        for (int i = 0; i < subArrayLength && to >= from; i++, from++) {
+            subArray[i] = inputArray[from];
+        }
+        return subArray;
+    }
+
+}
+
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "data=" + data +
+                "}";
+    }
+}
---
+++ src/test/java/net/learning/computerscience/datastructures/topic_6_trees/ideserve/p49_max_element_for_each_subarray_of_size_k/SolutionTest.java
@@ -0,0 +1,35 @@
+package net.learning.computerscience.datastructures.topic_6_trees.ideserve.p49_max_element_for_each_subarray_of_size_k;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    // WARNING: AVL BALANCING NOT CORRECTLY IMPLEMENTED
+
+
+
+    @Test
+    public void getMaxElements() {
+        int[] inputArray = {4,2,12,34,23,35,44,55};
+        assertThat(Solution.getMaxElements(inputArray, 3), is(equalTo(Arrays.asList(12,34,34,35,44,55))));
+    }
+
+    @Test
+    public void getMaxElements2() {
+        int[] inputArray = {4,2,12,34,23,35,44,55};
+        assertThat(Solution.getMaxElements(inputArray, 5), is(equalTo(Arrays.asList(34,35,44,55))));
+    }
+
+    @Test
+    public void getMaxElements3() {
+        int[] inputArray = {4,2,12,34,4,23,4,35,44,2,12,55};
+        assertThat(Solution.getMaxElements(inputArray, 6), is(equalTo(Arrays.asList(34,34,35,44,44,44,55))));
+    }
+
+}
